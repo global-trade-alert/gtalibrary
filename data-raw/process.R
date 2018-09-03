f74d2ceda0a2b4d84322cbb68728ab9826b5aa55
patrickbuess <mail@patrickbuess.ch>
2018-09-03 23:48:38 +0200

    - Created data-raw folder, where the dataframe creation and processing file is placed

---
+++ data-raw/process.R
@@ -0,0 +1,10 @@
+## Create necessary dataframes for function
+
+# load csv
+cpc.names <- read.csv("../R help files/cpc names.csv", sep=";")
+hs.names <- read.csv("../R help files/hs2012 names.csv", sep=";")
+cpc.to.hs <- read.csv("../R help files/cpc 2.1 to HS 2012.csv", sep=";")
+
+# Saving datasets in the a sysdata.R file in the R folder. This way it will be only available inside the package.
+# Call the internally saved packages from inside the function like this: gtalibrary:::name_of_dataframe
+devtools::use_data(cpc.names, hs.names, cpc.to.hs, internal = T, overwrite = T)
