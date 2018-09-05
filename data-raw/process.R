d75b0cf012e571db5c4a2a3b42681847735edd83
patrickbuess <mail@patrickbuess.ch>
2018-09-05 16:22:33 +0200

    - Changes in process.R, renaming dataframe columnd - created data.R file, containing dataframe documentations - Update to sysdata.rda

--- data-raw/process.R
+++ data-raw/process.R
@@ -2,8 +2,11 @@
 
 # load csv
 cpc.names <- read.csv("../R help files/cpc names.csv", sep=";")
+
 hs.names <- read.csv("../R help files/hs2012 names.csv", sep=";")
+
 cpc.to.hs <- read.csv("../R help files/cpc 2.1 to HS 2012.csv", sep=";")
+colnames(cpc.to.hs) <- c("cpc","hs")
 
 # Saving datasets in the a sysdata.R file in the R folder. This way it will be only available inside the package.
 # Call the internally saved packages from inside the function like this: gtalibrary:::name_of_dataframe
