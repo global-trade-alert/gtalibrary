199a687a84bf0f715dc1a7b000d7f2e2bac04502
patrickbuess <mail@patrickbuess.ch>
2018-11-15 20:39:03 +0100

    - add range.split and legend.labels functionality to plot map function

--- R/gta plot map.R
+++ R/gta plot map.R
@@ -9,8 +9,9 @@
 #' @param value Specify the name of the column containing the value for each country.
 #' @param colour.low Specifiy a color from the gta colour palette for the low part of the gradient. Default: GTA dark blue
 #' @param colour.high Specify a color from the gta colour palette for the high part of the gradient. Default: GTA light blue
-#' @param colour.breaks Specify the breaks for the colour gradient. Default: ggplot automatic.
+#' @param range.split Specify the number of parts the value range should be split to. Either as integer (range will be automatically divided and sequenced) or as sequence (e.g. seq(200,1000,200)). Default: ggplot automatic.
 #' @param legend.title Specify a title for the colour legend. Default: No title.
+#' @param legend.labels Specify the legend labels as list. Default: ggplot automatic.
 #' @param title Specify a title for the map plot. Default: No title.
 #'
 #' @references www.globaltradealert.org
@@ -22,8 +23,9 @@ gta_plot_map <- function(data = NULL,
                          value = NULL,
                          colour.low = gta_colour$blue[4],
                          colour.high = gta_colour$blue[1],
-                         colour.breaks = waiver(),
+                         range.split = waiver(),
                          legend.title = NULL,
+                         legend.labels = waiver(),
                          title = NULL) {
 
   library("data.table")
@@ -44,12 +46,23 @@ gta_plot_map <- function(data = NULL,
   world <-  world[with(world, order(X)),]
   world$value[is.na(world$value) == T] <- 0
 
+  if (length(range.split)==1){
+  eval(parse(text=paste("range.split <- seq(",min(world$value),",",max(world$value),",",max(world$value)/range.split,")",sep="")))
+    range.split <- range.split[2:length(range.split)]
+    if(length(legend.labels)>0) {
+      if(length(range.split)!=length(legend.labels)) {
+        stop("Please make sure that range.split and legend.labels have the same number of elements.")
+      }
+    }
+  }
+
+
   plot = ggplot() +
     geom_polygon(data= subset(world, country != "Antarctica"), aes(x = long, y = lat, group = group, fill = value), size = 0.2, color = "white") +
     coord_fixed() + # Important to fix world map proportions
     ggtitle(title) +
     labs(x="", y="") +
-    scale_fill_gradient(low = colour.low, high = colour.high, breaks=colour.breaks, position="bottom") + # Set color gradient
+    scale_fill_gradient(low = colour.low, high = colour.high, breaks=range.split, position="bottom", labels=legend.labels) + # Set color gradient
     theme(axis.title.x=element_blank(),
           axis.text.x=element_blank(),
           axis.ticks.x=element_blank(),
@@ -59,8 +72,9 @@ gta_plot_map <- function(data = NULL,
           panel.background = element_blank(),
           legend.position = "bottom",
           plot.title = element_text(family = "", colour = "#333333", size = 11, hjust = 0.5, margin = margin(b=10)),
-          legend.title = element_text(vjust= 0.2, family="", colour = "#333333", size = 11*0.8, margin = margin(r=10)),
-          legend.text = element_text(family="", colour = "#333333", size = 11*0.8)
+          legend.title = element_text(vjust= 0.3, family="", colour = "#333333", size = 11*0.8, margin = margin(r=10)),
+          legend.text = element_text(family="", colour = "#333333", size = 11*0.8, angle = 0, hjust=0, vjust=0, margin = margin(r=10)),
+          legend.text.align = 0
 
     ) +
     guides(fill=guide_legend(title=legend.title, label.position = "top"),
