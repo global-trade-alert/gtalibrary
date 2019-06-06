9bd4d64f35b9a67efff697a2c043967d5deb5d16
patrickbuess <mail@patrickbuess.ch>
2019-06-06 13:59:13 +0200

    - replaced device for transparency EPSs because earlier one did not work

--- R/gta plot saver.R
+++ R/gta plot saver.R
@@ -11,7 +11,7 @@
 #' @param width Takes in a specific plot height. If unspecified, height will be calculated through aspect.ratio.
 #' @param png Set png output to FALSE. Default: TRUE.
 #' @param eps Set eps output to FALSE. Default: TRUE.
-#' @param eps Set cairo_ps output to TRUE if you want to generate an eps file with transparency levels. Please make sure "Cairo" package is installed on your machine. Default: FALSE.
+#' @param eps Set cairo_ps output to TRUE if you want to generate an eps file with transparency levels. Default: FALSE.
 #' @param aspect.ratio Define the aspect ratio of the plot as integer. Width = 1, height = 1*aspect.ratio. If height is defined, aspect.ratio does have no effect.
 #'
 #'
@@ -54,10 +54,9 @@ gta_plot_saver <- function(plot = NULL,
            units = "cm")
   }
   if (cairo_ps == T) {
-    library("Cairo")
     ggsave(filename=paste0(name,".eps"),
            plot=plot,
-           device = cairo_ps,
+           device = grDevices::cairo_ps,
            path=path,
            width=width,
            height = height,
