8bbe15eebd4264e6c794340a32f52170da2c9d78
patrickbuess <mail@patrickbuess.ch>
2019-09-09 13:24:45 +0200

    - adjust parameter choice output wording for cpc sectors, standardize for better handling the pm choices when setting up data kitchen excel file

--- R/gta data slicer.R
+++ R/gta data slicer.R
@@ -1047,13 +1047,13 @@ gta_data_slicer=function(data.path = "data/master_plus.Rdata",
             master.temp=subset(master.temp, as.numeric(affected.sector) %in% cpc.sectors)
 
             parameter.choices=rbind(parameter.choices,
-                                    data.frame(parameter="CPC codes included:", choice=paste(cpc.sectors, collapse = ", ")))
+                                    data.frame(parameter="CPC sectors included:", choice=paste(cpc.sectors, collapse = ", ")))
 
           } else if (keep.cpc==F){
             master.temp=subset(master.temp, ! as.numeric(affected.sector) %in% cpc.sectors)
 
             parameter.choices=rbind(parameter.choices,
-                                    data.frame(parameter="CPC codes included:", choice=paste("All except ", paste(cpc.sectors, collapse = ", "), sep="")))
+                                    data.frame(parameter="CPC sectors included:", choice=paste("All except ", paste(cpc.sectors, collapse = ", "), sep="")))
 
           }
 
