7e435a72bdac33490705b7c86d72121e45e25b9c
patrickbuess <mail@patrickbuess.ch>
2019-10-02 15:01:45 +0200

    - New function gta_pwd() getting credentials from a dropbox file and returning them as a list.

---
+++ R/gta pwd.R
@@ -0,0 +1,30 @@
+# Roxygen documentation
+
+#' GTA pwd function
+#'
+#' Returns a list of GTA credentials.
+#'
+#' @param type If parameter type is specified, a single list will be returned. Otherwise all credentials will be returned. Default: All.
+
+#' @return Outputs either a list of credentials for a specified app or all credentials available for GTA.
+#' @references www.globaltradealert.org
+#' @author Global Trade Alert
+
+gta_pwd <- function(type=NULL) {
+
+  # Load stored credentials
+  source("setup/pwd.R")
+
+  if(is.null(type)) {
+    eval(parse(text=paste0("creds <- list(",paste0(credentials," = ",credentials, collapse=", "),")")))
+    return(creds)
+  } else {
+    if (! tolower(type) %in% tolower(credentials)) {
+      stop("Credentials type not found")
+    } else {
+      eval(parse(text=paste0("creds <- ",credentials[tolower(credentials) == tolower(type)])))
+      return(creds)
+    }
+  }
+
+  }
