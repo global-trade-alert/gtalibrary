3bba95a6c18d3221313a54a6289547470fda7f1e
patrickbuess <mail@patrickbuess.ch>
2020-01-10 15:00:25 +0100

    - added new function to ease creation of map ready dfs

---
+++ R/gta plot map df.R
@@ -0,0 +1,37 @@
+# Roxygen documentation
+
+#' Map geo data to a dataframe with un codes.
+#'
+#' This function facilitates map creation with ggplot.
+#'
+#' @param data Specify the dataframe with the relevant columns.
+#' @param countries Specify the name of the column containing the countries.
+#' @param values Specify the name of the column containing the value for each country.
+#'
+#' @references www.globaltradealert.org
+#' @author Global Trade Alert
+
+
+gta_plot_map_df <- function(data = vdma.master,
+                         countries = NULL,
+                         values = NULL
+                         ) {
+  
+if (is.null(data) | is.null(countries) | is.null(values)) {
+  stop("Please define column names (countries and values) and a dataframe!")
+}
+  
+world <- gtalibrary::world.geo
+
+data[,c("UN","value")] <- data[,c(countries,values)]
+data$UN <- gta_un_code_vector(data$UN)
+
+# merge data with map data
+world = merge(world, data[,c("UN","value")], by="UN", all.x=T)
+
+###### IMPORTANT, sort for X (id) again
+world <-  world[with(world, order(X)),]
+
+return(world)
+
+}
