3ff032fa65a6f53e4b7332489631f1f47c4d4318
Johannes Fritz <fritz.johannes@gmail.com>
2021-09-15 16:37:44 +0200

    new function to store local copy of GTA data file

---
+++ R/gta cloud update.R
@@ -0,0 +1,36 @@
+# Roxygen documentation
+
+#' Update your local copy of our data set.
+#'
+#' @return Be up to date with our latest data.
+#' @references www.globaltradealert.org
+#' @author Global Trade Alert
+
+gta_cloud_update = function(){
+  library(httr)
+  zip.file.name="cloud update.zip"
+  print("Be sure to set your working directory to your local GTA folder.")
+  
+  user.pwd= .rs.askForPassword("Please enter your password to start the download.")
+  
+  source("setup/gta_cloud_download.R")
+  if(user.pwd==download.passphrase){
+    
+    print("Starting download ...")
+    GET("https://www.dropbox.com/s/znk3kcmohbp36s5/GTA%20cloud.zip?dl=1", write_disk(zip.file.name, overwrite=TRUE))
+    print("... download complete ...")
+    
+    unzip(zip.file.name)
+    print("... extracted ...")
+    unlink(zip.file.name)
+    print("... all cleaned up.")
+    print("Enjoy!")
+    
+  } else {
+    
+    print("Wrong password.")
+    
+  }
+
+}
+
