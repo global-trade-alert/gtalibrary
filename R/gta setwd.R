6c531b18f5b06e98d80390cd047927bcbeeca76f
Johannes Fritz <fritz.johannes@gmail.com>
2021-11-04 09:06:01 +0100

    updated to GDRIVE

--- R/gta setwd.R
+++ R/gta setwd.R
@@ -6,13 +6,21 @@
 #' @references www.globaltradealert.org
 #' @author Global Trade Alert
 
-gta_setwd = function(x){
+gta_setwd = function(gdrive=NA){
 
-  split.path=unlist(strsplit(getwd(),"/"))
-  cld.position=which(split.path=="GTA root")
-  if(length(cld.position)==0) cld.position=which(split.path=="GTA cloud")
-  if(cld.position<length(split.path)){
-    setwd(paste(rep("..", length(split.path)-cld.position), collapse="/"))
+  if(is.na(gdrive)){
+
+    split.path=unlist(strsplit(getwd(),"/"))
+    cld.position=which(split.path=="GTA root")
+    if(length(cld.position)==0) cld.position=which(split.path=="GTA cloud")
+    if(cld.position<length(split.path)){
+      setwd(paste(rep("..", length(split.path)-cld.position), collapse="/"))
+    }
+
+  } else {
+
+    setwd(paste0(gdrive, ":/.shortcut-targets-by-id/17FRvsAcpS6vSAA-JfUUBOS0ixF9FSuio/GTA cloud"))
+
   }
 
 }
