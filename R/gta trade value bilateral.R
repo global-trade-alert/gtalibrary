aa0581022495ad7dbba3d56e000edf40a85f84f7
svensglinz <svenglinz@gmail.com>
2023-02-19 12:16:41 +0100

    changes

--- R/gta trade value bilateral.R
+++ R/gta trade value bilateral.R
@@ -25,45 +25,40 @@
 
 # Function infos and parameters  --------------------------------------------
 #' @export
-gta_trade_value_bilateral <- function(
-    importing.country = NULL, keep.importer = NULL,
-    exporting.country = NULL, keep.exporter = NULL,
-    cpc.sectors=NULL, keep.cpc=TRUE, hs.codes = NULL,
-    keep.hs = TRUE, trade.data="base",
-    trade.data.path = "data/support tables/Goods support table for gtalibrary.Rdata",
-    trade.data.file = NULL # ensure that this data.frame can also be fed from memory for better performance in loops!
+gta_trade_value_bilateral <- function(importing.country = NULL, keep.importer = NULL,
+                                      exporting.country = NULL, keep.exporter = NULL,
+                                      cpc.sectors = NULL, keep.cpc = TRUE, hs.codes = NULL,
+                                      keep.hs = TRUE, trade.data = "base",
+                                      trade.data.path = "data/support tables/Goods support table for gtalibrary.Rdata",
+                                      trade.data.file = NULL # ensure that this data.frame can also be fed from memory for better performance in loops!
 ) {
-
     gtalibrary::gta_parameter_check(trade.data, c("base", "prior year", "current year", "before announcement", "during announcement", as.character(2005:2020)))
     filter_statement <- vector("character")
 
     if (trade.data == "base") {
         trade.base <- gtalibrary::trade.base |>
             dplyr::mutate(trade.value = trade.value / 3)
-
     } else if (is.null(trade.data.file)) {
-        trade.base <- as.data.table(readRDS(trade.data.path))
+        trade.base <- data.table::as.data.table(readRDS(trade.data.path))
     } ## convert to data.table (analogous to gta.data.slicer())
 
-    if(trade.data %in% as.character(2005:2020)) {
-      base_year <- as.numeric(trade.data)
-      trade.base <- trade.base |>
-          dplyr::filter(year == base_year)
-
+    if (trade.data %in% as.character(2005:2020)) {
+        base_year <- as.numeric(trade.data)
+        trade.base <- trade.base |>
+            dplyr::filter(year == base_year)
     }
 
-    if(tolower(trade.data) == "prior year") {
+    if (tolower(trade.data) == "prior year") {
         trade.base <- trade.base |>
             dplyr::mutate(year = year + 1)
-      }
+    }
 
-     ## importer
-    if(!is.null(importing.country)){
-
+    ## importer
+    if (!is.null(importing.country)) {
         gtalibrary::gta_logical_check(keep.importer, is.logical)
         importers_filter <- gtalibrary::gta_un_code_vector(importing.country)
 
-        if(keep.importer){
+        if (keep.importer) {
             filter_statement <- append(filter_statement, "i.un %in% importers_filter")
         } else {
             filter_statement <- append(filter_statement, "!i.un %in% importers_filter")
@@ -71,49 +66,43 @@ gta_trade_value_bilateral <- function(
     }
 
     ## exporter
-    if(!is.null(exporting.country)){
-
+    if (!is.null(exporting.country)) {
         gtalibrary::gta_logical_check(keep.exporter, is.logical)
         exporters_filter <- gtalibrary::gta_un_code_vector(exporting.country)
-        if(keep.exporter) {
+        if (keep.exporter) {
             filter_statement <- append(filter_statement, "a.un %in% exporters_filter")
         } else {
             filter_statement <- append(filter_statement, "a.un %in% exporters_filter")
         }
 
-  ## hs codes
-    if(!is.null(hs.codes)){
+        ## hs codes
+        if (!is.null(hs.codes)) {
+            gtalibrary::gta_logical_check(keep.hs, is.logical)
+            hs_codes_filter <- gtalibrary::gta_hs_code_check(codes = hs.codes, message = FALSE)
 
-        gtalibrary::gta_logical_check(keep.hs, is.logical)
-        hs_codes_filter <- gtalibrary::gta_hs_code_check(codes = hs.codes, message = FALSE)
-
-        if (keep.hs){
-            filter_statement <- append(filter_statement, "hs6 %in% hs_codes_filter") ## check if we need to pad the codes or make them numeric!!!
-        } else {
-            filter_statement <- append(filter_statement, "!hs6 %in% hs_codes_filter") 
+            if (keep.hs) {
+                filter_statement <- append(filter_statement, "hs6 %in% hs_codes_filter") ## check if we need to pad the codes or make them numeric!!!
+            } else {
+                filter_statement <- append(filter_statement, "!hs6 %in% hs_codes_filter")
+            }
         }
-  }
 
-    ## cpc codes
-    if (!is.null(cpc.sectors)) {
-        gtalibrary::gta_logical_check(keep.cpc, is.loical)
-        cpc_codes_filter <- gtalibrary::gta_cpc_to_hs(cpc.sectors)
+        ## cpc codes
+        if (!is.null(cpc.sectors)) {
+            gtalibrary::gta_logical_check(keep.cpc, is.loical)
+            cpc_codes_filter <- gtalibrary::gta_cpc_to_hs(cpc.sectors)
 
-        if (is.null(heep.cpc)) {
-            filter_statement <- append(filter_statement, "hs6 %in% cpc_codes_filter")
-        } else {
-            filter_statement <- append(filter_statement, "!hs6 %in% cpc_codes_filter")
+            if (is.null(heep.cpc)) {
+                filter_statement <- append(filter_statement, "hs6 %in% cpc_codes_filter")
+            } else {
+                filter_statement <- append(filter_statement, "!hs6 %in% cpc_codes_filter")
+            }
         }
+
+        # filter dataset
+        trade.base |>
+            dplyr::filter(eval(parse(text = filter_statement))) |>
+            data.table::as.data.table()
     }
-
-    # filter dataset
-    trade.base |>
-        dplyr::filter(eval(parse(text = filter_statement))) |>
-        data.table::as.data.table()
-  }
 }
-gtalibrary::gta_trade_value_bilateral(
-    importing.country = c("Switzerland", "Japan"), keep.importer = TRUE
-)
-
 ## could specify the year value here --> WOuld make it easier int he trade coverage function!
