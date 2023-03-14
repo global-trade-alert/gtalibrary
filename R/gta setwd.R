3ba6573af3bc4c46bf8b62ffcd15ce00ed523bc5
svensglinz <svenglinz@gmail.com>
2023-03-15 00:17:22 +0100

    changes

--- R/gta setwd.R
+++ R/gta setwd.R
@@ -2,38 +2,28 @@
 
 #' Sets your working directory to that used by the GTA data team.
 #'
-#' @return A common working directory.
-#' @references www.globaltradealert.org
-#' @author Global Trade Alert
+#' @return A common working directory
 #' @export
-gta_setwd = function(gdrive=NA){
-
-  if(is.na(gdrive)){
-
-    split.path=unlist(strsplit(getwd(),"/"))
-    cld.position=which(split.path=="GTA root")
-    if(length(cld.position)==0) cld.position=which(split.path=="GTA cloud")
-    if(cld.position<length(split.path)){
-      setwd(paste(rep("..", length(split.path)-cld.position), collapse="/"))
+gta_setwd <- function(gdrive = NA) {
+    if (is.na(gdrive)) {
+        split_path <- unlist(strsplit(getwd(), "/"))
+        cld_position <- which(split_path == "GTA root")
+        if (length(cld_position) == 0) cld_position <- which(split_path == "GTA cloud")
+        if (cld_position < length(split_path)) {
+            setwd(paste(rep("..", length(split_path) - cld_position), collapse = "/"))
+        }
+    } else {
+        gdrive_gta_cloud <- "/.shortcut-targets-by-id/17FRvsAcpS6vSAA-JfUUBOS0ixF9FSuio/GTA cloud"
+        if (!is.null(Sys.info())) {
+            if (Sys.info()["sysname"] == "Darwin") { # set wd for mac osx
+                setwd(paste0(gdrive, gdrive_gta_cloud))
+            } else if (Sys.info()["sysname"] == "Linux") {
+                setwd(gdrive)
+            } else { # set wd for windows
+                setwd(paste0(gdrive, ":", gdrive_gta_cloud))
+            }
+        } else { # fallback to windows
+            setwd(paste0(gdrive, ":", gdrive_gta_cloud))
+        }
     }
-
-  } else {
-
-    gdrive.gta_cloud <- "/.shortcut-targets-by-id/17FRvsAcpS6vSAA-JfUUBOS0ixF9FSuio/GTA cloud"
-
-    if(!is.null(Sys.info())) {
-      if(Sys.info()['sysname'] == "Darwin") {  # set wd for mac osx
-        setwd(paste0(gdrive, gdrive.gta_cloud))
-      } else if (Sys.info()['sysname'] == "Linux") {
-        setwd(gdrive)
-      } else {  # set wd for windows
-        setwd(paste0(gdrive, ":", gdrive.gta_cloud))
-      }
-    } else {  # fallback to windows
-      setwd(paste0(gdrive, ":", gdrive.gta_cloud))
-    }
-
-  }
-
 }
-
