b413e65c9c1122c9f47400d921a7f5b5b267a7ee
svensglinz <svenglinz@gmail.com>
2023-03-14 23:15:00 +0100

    changes

--- R/gta hs code check.R
+++ R/gta hs code check.R
@@ -10,61 +10,53 @@
 #' @author Global Trade Alert
 
 #' @export
-gta_hs_code_check=function(codes){
-
+gta_hs_code_check <- function(codes) {
   # Load HS names, create 4 and 2 digits columns
   hs.names <- gtalibrary::hs.names
 
-  hs.codes=c(unique(hs.names$HS12code),
-             unique(substr(hs.names$HS12code[(nchar(hs.names$HS12code)%%2)==0],1,2)),
-             unique(substr(hs.names$HS12code[(nchar(hs.names$HS12code)%%2)!=0],1,1)),
-             unique(substr(hs.names$HS12code[(nchar(hs.names$HS12code)%%2)==0],1,4)),
-             unique(substr(hs.names$HS12code[(nchar(hs.names$HS12code)%%2)!=0],1,3)))
+  hs.codes <- c(
+    unique(hs.names$HS12code),
+    unique(substr(hs.names$HS12code[(nchar(hs.names$HS12code) %% 2) == 0], 1, 2)),
+    unique(substr(hs.names$HS12code[(nchar(hs.names$HS12code) %% 2) != 0], 1, 1)),
+    unique(substr(hs.names$HS12code[(nchar(hs.names$HS12code) %% 2) == 0], 1, 4)),
+    unique(substr(hs.names$HS12code[(nchar(hs.names$HS12code) %% 2) != 0], 1, 3))
+  )
 
 
-  if (is.numeric(codes)==T) {
-
+  if (is.numeric(codes) == T) {
     if (max(nchar(codes)) >= 7) {
-      print(paste0("Codes with more than 6 figures provided. These will be reduced: ", paste0(codes[nchar(codes)>=7], collapse=", ")))
+      print(paste0("Codes with more than 6 figures provided. These will be reduced: ", paste0(codes[nchar(codes) >= 7], collapse = ", ")))
 
       # taking the first 6 digits from numbers with even digit count, and only the first 5 digits of the others.
-      codes <- unique(c(as.numeric(substr(codes[(nchar(codes)%%2)==0], 1,6)),as.numeric(substr(codes[(nchar(codes)%%2)!=0], 1,5))))
+      codes <- unique(c(as.numeric(substr(codes[(nchar(codes) %% 2) == 0], 1, 6)), as.numeric(substr(codes[(nchar(codes) %% 2) != 0], 1, 5))))
     }
 
 
-    if (sum(as.numeric((codes %in% hs.codes)==F))>0){
-      print(paste0("Non existing values provided: ", paste0(codes[(codes %in% hs.codes)==F], collapse = ", ")))
-
-
+    if (sum(as.numeric((codes %in% hs.codes) == F)) > 0) {
+      print(paste0("Non existing values provided: ", paste0(codes[(codes %in% hs.codes) == F], collapse = ", ")))
     } else {
-
       print("Conversion successful. Returning vector of 6-digit HS codes.")
 
       ## creating a vector of all 6-digit codes
 
-      if(length(codes[nchar(codes)%%2==0])>0 & length(codes[nchar(codes)%%2!=0])>0){
-        codes=c(unique(hs.names$HS12code[nchar(hs.names$HS12code)%%2==0])[substr(unique(hs.names$HS12code[nchar(hs.names$HS12code)%%2==0]),1,nchar(codes[nchar(codes)%%2==0])) %in% codes[nchar(codes)%%2==0]],
-                unique(hs.names$HS12code[nchar(hs.names$HS12code)%%2!=0])[substr(unique(hs.names$HS12code[nchar(hs.names$HS12code)%%2!=0]),1,nchar(codes[nchar(codes)%%2!=0])) %in% codes[nchar(codes)%%2!=0]])
-
+      if (length(codes[nchar(codes) %% 2 == 0]) > 0 & length(codes[nchar(codes) %% 2 != 0]) > 0) {
+        codes <- c(
+          unique(hs.names$HS12code[nchar(hs.names$HS12code) %% 2 == 0])[substr(unique(hs.names$HS12code[nchar(hs.names$HS12code) %% 2 == 0]), 1, nchar(codes[nchar(codes) %% 2 == 0])) %in% codes[nchar(codes) %% 2 == 0]],
+          unique(hs.names$HS12code[nchar(hs.names$HS12code) %% 2 != 0])[substr(unique(hs.names$HS12code[nchar(hs.names$HS12code) %% 2 != 0]), 1, nchar(codes[nchar(codes) %% 2 != 0])) %in% codes[nchar(codes) %% 2 != 0]]
+        )
       } else {
-
-        if(length(codes[nchar(codes)%%2==0])>0 & length(codes[nchar(codes)%%2!=0])==0){
-          codes=c(unique(hs.names$HS12code[nchar(hs.names$HS12code)%%2==0])[substr(unique(hs.names$HS12code[nchar(hs.names$HS12code)%%2==0]),1,nchar(codes[nchar(codes)%%2==0])) %in% codes[nchar(codes)%%2==0]])
-
+        if (length(codes[nchar(codes) %% 2 == 0]) > 0 & length(codes[nchar(codes) %% 2 != 0]) == 0) {
+          codes <- c(unique(hs.names$HS12code[nchar(hs.names$HS12code) %% 2 == 0])[substr(unique(hs.names$HS12code[nchar(hs.names$HS12code) %% 2 == 0]), 1, nchar(codes[nchar(codes) %% 2 == 0])) %in% codes[nchar(codes) %% 2 == 0]])
         } else {
-          codes=c(unique(hs.names$HS12code[nchar(hs.names$HS12code)%%2!=0])[substr(unique(hs.names$HS12code[nchar(hs.names$HS12code)%%2!=0]),1,nchar(codes[nchar(codes)%%2!=0])) %in% codes[nchar(codes)%%2!=0]])
-
+          codes <- c(unique(hs.names$HS12code[nchar(hs.names$HS12code) %% 2 != 0])[substr(unique(hs.names$HS12code[nchar(hs.names$HS12code) %% 2 != 0]), 1, nchar(codes[nchar(codes) %% 2 != 0])) %in% codes[nchar(codes) %% 2 != 0]])
         }
-
       }
 
       return(codes)
-
     }
-  } else if (is.numeric(codes)==F) {
-      stop("Please input all codes as integer!")
+  } else if (is.numeric(codes) == F) {
+    stop("Please input all codes as integer!")
   }
 
   rm(codes)
 }
-
